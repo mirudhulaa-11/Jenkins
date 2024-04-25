072eb84df63f91fa424505719877d5459806424f
mirudhulaa-11 <mirudhulaa.m2021@vitstudent.ac.in>
2024-04-25 12:09:08 +0530

    First Commit

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+public class Factorial {
+    public static void main(String[] args) {
+        int number = 6; 
+        long factorial = calculateFactorial(number);
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+
+    public static long calculateFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+}
